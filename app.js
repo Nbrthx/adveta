34ad6686cbb16f43ca629cbb9cb793437824175d
Nbrthx <78901899+Nbrthx@users.noreply.github.com>
2022-02-01 06:49:58 +0700

    Update app.js

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ app.use(express.static(path.join(__dirname, "public")))
 
 app.get("/", (req, res) => {
   res.sendFile("index.html")
-}
+})
 
 app.listen(port, () => {
   console.log("Listening on port "+port)
-}
+})
