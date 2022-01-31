bcb181bf8dccaae602b8f8a058a91b73ad067c98
Nbrthx <78901899+Nbrthx@users.noreply.github.com>
2022-02-01 06:37:59 +0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const path = require("path")
+const port = process.env.PORT || 3000
+
+var app = express()
+
+app.use(express.static(path.join(__dirname, "public")))
+
+app.get("/", (req, res) => {
+  res.sendFile("index.html")
+}
+
+app.listen(port, () => {
+  console.log("Listening on port "+port)
+}
