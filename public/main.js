46ba1a147ce774b5760af253681d733577c2f4a2
Nbrthx <78901899+Nbrthx@users.noreply.github.com>
2022-02-01 09:51:56 +0700

    Update main.js

--- public/main.js
+++ public/main.js
@@ -13,18 +13,15 @@ var config = {
 var game = new Phaser.Game(config)
 
 function preload(){
-    console.log("game: "+game)
-    console.log("preload: "+this)
     this.load.spritesheet("vguy", "https://iili.io/l48pta.png", { frameWidth: 32, frameHeight: 32 })
 }
 
 var vguy;
 
 function create(){
-    console.log("create: "+this)
     this.anims.create({
         key: "idle",
-        frameRate: 16,
+        frameRate: 20,
         frames: this.anims.generateFrameNumbers("vguy", { start: 0, end: 10 }),
         repeat: -1
     })
